906454bf50ceebe72013688eea8a7293dcab6f35
gyuhwanhwang <rbghks0327@gmail.com>
2019-12-22 21:43:09 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
-// const express = require("express");
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
+// default 설정을 안해줬기 때문에 콕집어서 데려옴
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on: http://localhost:${PORT}`);
-
-// const handleHome(req, res) {
-//     res.send("hello from home");
-// }
 
 const handleHome = (req, res) => res.send("hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-// const betweenHome = (req, res, next) => {
-//     console.log("between");
-//     next();
-// };
-// app.use(betweenHome);
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -35,4 +21,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); //get이 아닌 use사용, /user를 통해 들어온것은 userRouter 사용하겠다
+
+export default app; // 파일을 불렀을때 app object를 주겠다
