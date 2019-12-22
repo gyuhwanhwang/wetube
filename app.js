813ffbcba5428dc92e90ff65657cd5590c894c95
gyuhwanhwang <rbghks0327@gmail.com>
2019-12-22 22:41:45 +0900

    #2.10 MVC Pattern 2 routes.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRoter";
 import globalRouter from "./routers/globalRouter";
 // default 설정을 안해줬기 때문에 콕집어서 데려옴
+import routes from "./routes";
 const app = express();
 
 app.use(cookieParser());
@@ -15,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // for securerity
 app.use(morgan("dev")); // for logging
 
-app.use("/", globalRouter);
-app.use("/user", userRouter); //get이 아닌 use사용, /user를 통해 들어온것은 userRouter 사용하겠다
-app.use("video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter); //get이 아닌 use사용, /user를 통해 들어온것은 userRouter 사용하겠다
+app.use(routes.videos, videoRouter);
 
 export default app; // 파일을 불렀을때 app object를 주겠다
