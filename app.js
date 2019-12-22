05e308138c1679b7e7506a284d12b70bca0d739e
gyuhwanhwang <rbghks0327@gmail.com>
2019-12-22 22:17:11 +0900

    #2.9 MVC Pattern 1 router

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRoter";
+import globalRouter from "./routers/globalRouter";
 // default 설정을 안해줬기 때문에 콕집어서 데려옴
 const app = express();
-
-const handleHome = (req, res) => res.send("hello from home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // for securerity
 app.use(morgan("dev")); // for logging
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter); //get이 아닌 use사용, /user를 통해 들어온것은 userRouter 사용하겠다
+app.use("video", videoRouter);
 
 export default app; // 파일을 불렀을때 app object를 주겠다
