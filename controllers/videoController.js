adcb3ebee421001fad14228ae64cc9b422f30705
gyuhwanhwang <rbghks0327@gmail.com>
2019-12-28 23:59:19 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,24 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-    res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    // const searchingBy = req.query.term; // 예전 방식
+    const {
+        query: { term: searchingBy }
+    } = req; // 최신 방식
+    res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const videos = (req, res) =>
     res.render("videos", { pageTitle: "Video" });
+
 export const upload = (req, res) =>
     res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
     res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
     res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
     res.render("deletVideo", { pageTitle: "Delete Video" });
